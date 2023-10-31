e989845645b022913928b3aafcd7c635a04e6144
Jing Zhang <zhangjingbeijing@163.com>
2023-10-31 16:25:32 +0800

    Add files via upload

---
+++ LengthCDR3/igh_mutcodel_len.R
@@ -0,0 +1,50 @@
+library(stringr)
+
+load("/Users/wanglu/Documents/2021_TC/tb.RData")
+
+#Clinical information update
+c_info <- read.csv("/Users/wanglu/Documents/2021_TC/XZY_database/CGGA_clinical_TCRBCR.csv") 
+
+tmps <- cbind(Sample,intersect_data[,1:ncol(tmp)])  
+
+tmps <- tmps[which(tmps$CDR3.aa != "out_of_frame"),]
+tmps <- tmps[which(tmps$CDR3.aa != "partial"),]
+tmps <- tmps[which(tmps$CDR3.aa != "?"),]
+tmps <- tmps [!grepl("_",tmps$CDR3.aa),] 
+tmps <- tmps [!grepl("\\?",tmps$CDR3.aa),]
+
+#-----------------------------#
+#####---------mut codel----------#####
+mut_codel <- c_info[which(c_info$Sample =="IDH-MUT_X1p19q-Codel"),]
+category <-  tmps[which(tmps$CGGA_ID %in% mut_codel$CGGA_ID),] 
+
+Chain <- category[which(str_detect(category$V.name, "IGH")),] 
+Chain <- Chain[which(Chain$D.name!="*"),]
+Chain <- Chain[which(Chain$J.name!="*"),]  
+
+Chain <- Chain[,c("Sample","CGGA_ID","Clones","CDR3.aa","V.name","D.name","J.name")]
+Chain <- aggregate(x =Chain$Clones, by=list(Sample = Chain$Sample, CGGA_ID = Chain$CGGA_ID,
+                                            CDR3.aa = Chain$CDR3.aa, V.name = Chain$V.name,
+                                            D.name = Chain$D.name,
+                                            J.name = Chain$J.name),FUN=sum)
+colnames(Chain)[7] <- "Clones"
+
+Chain <- Chain[,-1]
+Chain <- split(Chain[,-1], Chain$CGGA_ID ) 
+Chain <- list(Chain)
+names(Chain) <- c("data")
+
+mutcodel <- data.frame(len = 0,prop = 0,CGGA_ID = 0)
+mutcodel <- mutcodel[-1,]
+for (i in 1:length(names(Chain$data))){
+  myvec <- Chain$data[[i]]
+  myvec$len <- nchar(myvec$CDR3.aa)
+  myvec$prop <- myvec$Clones/sum(myvec$Clones)
+  myvec <- myvec[,c("len","prop")]
+  myvec <- aggregate(x =myvec$prop, by=list(len = myvec$len),FUN=sum)
+  colnames(myvec)[2] <- "prop"
+  myvec$CGGA_ID <- names(Chain$data)[i]
+  mutcodel <-rbind(mutcodel,myvec)
+}
+mutcodel <- mutcodel[order(mutcodel$len),]
+write.csv(mutcodel,"/Users/wanglu/Documents/2021_TC/final_version2/length/igh_mutcodel_len.csv",row.names = FALSE ) 
